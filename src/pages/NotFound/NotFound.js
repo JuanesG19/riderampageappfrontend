1af5ff8ca3fa05a5a5ff1fb13ed815bf9c4d1617
Juan Esteban Gomez Roa <jgomezr@famisanar.com.co>
2023-08-18 18:10:53 -0500

    Login

--- src/pages/NotFound/NotFound.js
+++ src/pages/NotFound/NotFound.js
@@ -4,25 +4,8 @@ import "./styles.css";
 
 function NotFound() {
   return (
-    {/* <Box
-      className="container"
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        minHeight: "100vh",
-      }}
-    >
-      <div className="info">
-        <Typography variant="h1">404</Typography>
-        <Typography variant="h6">
-          La pagina que estas buscando no existe
-        </Typography>
-        <Link style={{ color: "white" }} to="/">
-          HomePage
-        </Link>
-      </div>
-    </Box> */}
+    <div>Hola</div>
+    
   );
 }
 
