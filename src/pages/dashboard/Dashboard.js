ae73d4bc34b1e83f79e1bd93df71516626f6a8ae
Juan Esteban Gomez Roa <jgomezr@famisanar.com.co>
2023-08-21 10:06:23 -0500

    Table

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,22 +1,71 @@
 import React, { useState } from "react";
 import Button from '@mui/material/Button';
 import Navbar from "../../components/navbar/Navbar";
+import Paper from '@mui/material/Paper';
 import { Link } from "react-router-dom";
 
 
 import "./styles.css";
+import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Typography } from "@mui/material";
 
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
 
 function Dashboard() {
+
+
   return (<>
     <Navbar />
+    <Box className="fullscreen-box">
+      <h1 className="text">Tabla de competidores</h1>
+    </Box>
+
     <div >
-      <Link to="/login" > 
-        <Button variant="contained" color="primary" sx={{mt: 2}}>
+      <Link to="/login" >
+        <Button variant="contained" color="primary" sx={{ mt: 2 }}>
           Login
         </Button>
       </Link>
     </div>
+
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   </>);
 }
 export default Dashboard;
